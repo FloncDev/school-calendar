27218e0db452c15aee2c37dc7c514ade672e27cb
FloncDev <FloncDev@users.noreply.github.com>
2023-09-06 02:54:08 +0100

    ✨ Create 1 event for double period instead of 2

--- src/lib.rs
+++ src/lib.rs
@@ -35,18 +35,36 @@ pub async fn get_calander() -> Option<NamedFile> {
 
     for (index, day) in week.iter().enumerate() {
         let classes = day.as_array()?;
-        for class in classes {
+        let mut skip = false;
+
+        for (class_index, class) in classes.iter().enumerate() {
+            if skip {
+                skip = false;
+                continue;
+            }
             let obj = class.as_object()?;
 
             let start_time = NaiveTime::parse_from_str(obj["start_time"].as_str()?, "%I:%M %p").unwrap();
-            let end_time = NaiveTime::parse_from_str(obj["end_time"].as_str()?, "%I:%M %p").unwrap();
+            let mut end_time = NaiveTime::parse_from_str(obj["end_time"].as_str()?, "%I:%M %p").unwrap();
+            
+            match classes.get(class_index + 1) {
+                Some(next_class) => {
+                    let next_obj = next_class.as_object()?;
+
+                    if next_obj["name"].as_str()? == obj["name"].as_str()? {
+                        end_time = NaiveTime::parse_from_str(next_obj["end_time"].as_str()?, "%I:%M %p").unwrap();
+                        skip = true;
+                    }
+                },
+                None => {}
+            }
 
             let now = Local::now();
             let now_date = now.date_naive();
-
+            
             let weekday = Weekday::try_from(index as u8).unwrap();
             let day = NaiveDate::from_isoywd_opt(now_date.year(), now_date.iso_week().week(), weekday)?;
-
+            
             let mut start_dt = NaiveDateTime::new(day, start_time);
             let mut end_dt = NaiveDateTime::new(day, end_time);
 
