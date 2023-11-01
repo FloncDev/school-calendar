52bb8c46086fdaf2a2a736ee2e689c21f747b217
FloncDev <floncdev@gmail.com>
2023-11-01 23:40:03 +0000

    :fire: Remove tests (hopefully for now)

--- tests/test.rs
+++
@@ -1,8 +0,0 @@
-use school_calander::{self, models::Homework};
-
-#[test]
-fn homework_shorthand() {
-    let hw = Homework::from_shorthand(String::from("CS 20/09 15 The rest of this is a summary"));
-
-    println!("{}", hw.summary);
-}
